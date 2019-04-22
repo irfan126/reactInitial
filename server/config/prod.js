31390e88246a11a09c0ae8ef7538424ca85aa551
irfan <irfan126@gmail.com>
2019-04-22 23:27:45 +0100

    Update config

--- server/config/prod.js
+++ server/config/prod.js
@@ -3,7 +3,9 @@ module.exports = {
   SECRET: process.env.SECRET,
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-  STRIPE_SK: process.env.STRIPE_SK
+  STRIPE_SK: process.env.STRIPE_SK,
+  GMAIL_USERNAME: process.env.GMAIL_USERNAME,
+  GMAIL_PASSWORD: process.env.GMAIL_PASSWORD
 }
 
 
