70255a7433409162fa5046ea853f4380c7472966
irfan <irfan126@gmail.com>
2019-04-28 17:48:17 +0100

    Update

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -29,24 +29,24 @@ class RentalDetail extends React.Component {
               <div className='col-md-6'>
               <Carousel showArrows={true} showThumbs={false} autoPlay={false} dynamicHeight={false}>
                 <div>
-                    {(rental.image1 === 'none') && <img  alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
-                    {!(rental.image1 === 'none') && <img alt='image1' src={rental.image1}/> }
+                    {(rental.image1 === 'none') && <img  style={{"height" : "400px", "width" : "540px"}} alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
+                    {!(rental.image1 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='image1' src={rental.image1}/> }
                 </div>
                 <div>
-                    {(rental.image2 === 'none') && <img alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
-                    {!(rental.image2 === 'none') && <img alt='image2' src={rental.image2}/> }
+                    {(rental.image2 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
+                    {!(rental.image2 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='image2' src={rental.image2}/> }
                 </div>
                 <div>
-                    {(rental.image3 === 'none') && <img alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
-                    {!(rental.image3 === 'none') && <img alt='image3' src={rental.image3}/> }
+                    {(rental.image3 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
+                    {!(rental.image3 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='image3' src={rental.image3}/> }
                 </div>
                 <div>
-                    {(rental.image4 === 'none') && <img alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
-                    {!(rental.image4 === 'none') && <img alt='image4' src={rental.image4}/> }
+                    {(rental.image4 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
+                    {!(rental.image4 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='image4' src={rental.image4}/> }
                 </div>
                 <div>
-                    {(rental.image5 === 'none') && <img alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
-                    {!(rental.image5 === 'none') && <img alt='image5' src={rental.image5}/> }
+                    {(rental.image5 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
+                    {!(rental.image5 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='image5' src={rental.image5}/> }
                 </div>
             </Carousel>
               </div>
