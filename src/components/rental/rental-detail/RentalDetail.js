f60ee36d0e8839d0b31abff21be8b363f30b32a5
irfan <irfan126@gmail.com>
2019-05-03 23:48:15 +0100

    Update

--- src/components/rental/rental-detail/RentalDetail.js
+++ src/components/rental/rental-detail/RentalDetail.js
@@ -31,8 +31,8 @@ class RentalDetail extends React.Component {
                 <div>
 
 
-                    {(rental.image1 === 'none') && <div className="cbs-Item" style={{"background-image": "url('https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365')"}}></div>}       
-                    {!(rental.image1 === 'none') && <div className="cbs-Item" style={{"background-image": `url(${rental.image1})`}}></div> }
+                    {(rental.image1 === 'none') && <img alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
+                    {!(rental.image1 === 'none') && <img alt='image1' src={rental.image1}/>}
                 </div>
                 <div>
                     {(rental.image2 === 'none') && <img style={{"height" : "400px", "width" : "540px"}} alt='imageNone' src={'https://s3.eu-west-2.amazonaws.com/bwm-image-dev/1553036875365'}/>}       
